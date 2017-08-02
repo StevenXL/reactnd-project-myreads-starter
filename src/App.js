91a2f97eb803cc1e6d091dd8e40a2b613cab4438
Steven Leiva <leiva.steven@gmail.com>
2017-08-02 19:18:10 -0400

    Use React Router to determine what to render.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import SearchPage from "./SearchPage";
 import BookShelf from "./BookShelf";
+import { Route } from "react-router-dom";
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  };
-
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? <SearchPage /> : <BookShelf />}
+        <Route path="/search" component={SearchPage} />
+        <Route exact path="/" component={BookShelf} />
       </div>
     );
   }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const SearchPage = () => {
+  console.log("in search");
   return (
     <div className="search-books">
       <div className="search-books-bar">
