41c5838c1b0cb7e80b2e695d131fdc849c2f6385
Steven Leiva <leiva.steven@gmail.com>
2017-08-03 18:43:16 -0400

    Render Book list dynamically in BookShelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 
 import Book from "./Book";
 
-const BookShelf = ({ title }) => {
+const BookShelf = ({ title, books }) => {
+  const bookElements = books.map(book => <Book key={book.id} {...book} />);
+
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">
@@ -11,35 +13,7 @@ const BookShelf = ({ title }) => {
       </h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <Book />
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 188,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">Ender's Game</div>
-              <div className="book-authors">Orson Scott Card</div>
-            </div>
-          </li>
+          {bookElements}
         </ol>
       </div>
     </div>
@@ -47,7 +21,8 @@ const BookShelf = ({ title }) => {
 };
 
 BookShelf.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired
 };
 
 export default BookShelf;
