678ad6c03b88dc857017ba996c1c0191aa8b4eeb
Steven Leiva <leiva.steven@gmail.com>
2017-08-04 15:29:47 -0400

    SearchPage searches via BooksAPI.

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -3,12 +3,28 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import BookShelf from "./BookShelf";
+import * as BooksAPI from "./BooksAPI";
 
 class SearchPage extends React.Component {
   state = { books: [] };
 
+  handleInput = event => {
+    const query = event.target.value;
+
+    if (query !== "") {
+      this.performQuery(query);
+    } else {
+      return null;
+    }
+  };
+
+  performQuery = query => {
+    BooksAPI.search(query).then(books => this.setState({ books: books }));
+  };
+
   render() {
     const { books } = this.state;
+    const handleInput = this.handleInput;
     const { moveBook } = this.props;
 
     return (
@@ -18,7 +34,11 @@ class SearchPage extends React.Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={handleInput}
+            />
           </div>
         </div>
         <div className="search-books-results">
